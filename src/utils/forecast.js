023555d651beabab4ee4c3e0a37181efea940676
Priyabrat <PriyabratSwain888@gmail.com>
2021-07-04 22:26:57 +0530

    Initial Commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback("Unabel to find location", undefined);
     } else {
-      callback(undefined, `${body.current.weather_descriptions[0]} It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out.`);
+      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`);
     }
   });
 };
